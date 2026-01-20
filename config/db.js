d716f6f0ad06dd32934de7beed13ab9c0b903e60
Kumba <kumbakhan21@gmail.com>
2026-01-20 18:29:48 +0000

    Updated db.js port configuration

--- config/db.js
+++ config/db.js
@@ -1,13 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.log("Database connection failed:", error.message);
-    process.exit(1);
-  }
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default connectDB;
